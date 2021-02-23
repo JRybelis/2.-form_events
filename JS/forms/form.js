03baaabcad983744c74d50e4877cdc210045d708
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-02-23 18:08:05 +0200

    keyboard event listener live feedback

--- JS/forms/form.js
+++ JS/forms/form.js
@@ -1,8 +1,9 @@
 const form = document.querySelector('.signup-form');
 // const username = document.querySelector('#username');
-
 const feedback = document.querySelector('.feedback');
+
 
 export {form}
 // export {username}
 export {feedback}
+
--- JS/main.js
+++ JS/main.js
@@ -2,13 +2,15 @@ import {form} from './forms/form.js';
 // import {username} from './forms/form.js';
 import {feedback} from './forms/form.js';
 
+const usernamePattern = /^[a-zA-Z]{6,12}$/;
+
 form.addEventListener('submit', e => {
     e.preventDefault();
     //console.log(username.value); optional, because form already has elements with #id that we can refer to from its event listener
     console.log (form.username.value); // as above
     //form validation:
     const usernameInput = form.username.value;
-    const usernamePattern = /^[a-zA-Z]{6,12}$/;
+    
     
     if (usernamePattern.test(usernameInput)) {
         //feedback good info
@@ -26,3 +28,13 @@ const pattern = /^[a-zA-Z]{6,}$/;
 let result = pattern.test(username); // returns boolean true or false.
 result = username.search(pattern); // does the check but returns an integer (for the position where the match was found). -1 if no match.
 
+
+//live feedback
+form.username.addEventListener('keyup', e => {
+    // console.log(e.target.value, form.username.value); // both can be used to obtain the keyboard input value
+    if (usernamePattern.test(e.target.value)){
+        form.username.setAttribute('class', 'success');
+    } else {
+        form.username.setAttribute('class', 'error');
+    }
+});
